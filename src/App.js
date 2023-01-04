5be302dc7391a18c9a35051757fa0dcc710b6cd0
sandy9601 <darshanamsandeep375@gmail.com>
2023-01-04 19:57:53 +0530

    frontend submit button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import React from'react';
+import ReactDOM from'react-dom';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [set,setdata]=useState(0)
+  const onclickHandler=()=>{
+
+  }
+
   return (
    <center>
     Hello world
+    <form>
+    <input type="text" placeholder='productName'/>
+    <input type="submit" value='submit'/>
+    </form>
    </center>
   );
 }
